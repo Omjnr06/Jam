c2d3c16e0445cb7598f5526c91eed852955f5b6c
Oladele Magbadelo Jr <omagbad@uwo.ca>
2026-07-06 22:50:01 +0300

    fix: fixed the SDK to work on phone app. Current available SDK is 57, SDK available for IOS testing is SDK 54

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,5 +1,6 @@
 import { useFonts } from 'expo-font';
-import { DarkTheme, DefaultTheme, Stack, ThemeProvider } from 'expo-router';
+import { Stack } from 'expo-router'; 
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'; 
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
@@ -7,16 +8,13 @@ import 'react-native-reanimated';
 import { useColorScheme } from '@/components/useColorScheme';
 
 export {
-  // Catch any errors thrown by the Layout component.
   ErrorBoundary,
 } from 'expo-router';
 
 export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
   initialRouteName: '(tabs)',
 };
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
@@ -24,7 +22,6 @@ export default function RootLayout() {
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
 
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
   useEffect(() => {
     if (error) throw error;
   }, [error]);
